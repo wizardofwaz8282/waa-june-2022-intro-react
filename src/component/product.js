256a81d11dbd92de5322d1fefe920ce6de06ce63
ElahehDaneshfar <elahehdaneshfar@gmail.com>
2022-07-01 16:27:47 -0500

    Update product

--- src/component/product.js
+++ src/component/product.js
@@ -1,6 +1,10 @@
 import React from "react";
-function product{
+function Product(obj){
     return(
-        
+        <div>
+        <h3>{obj.name}</h3>
+        <h3>{obj.price}</h3>
+        <h3>{obj.rating}</h3>
+    </div>
     )
 }
